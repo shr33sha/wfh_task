3113a3d245cce105871bd61938b5bb81117b6724
shr33sha <143892738+shr33sha@users.noreply.github.com>
2024-11-01 14:45:42 +0530

    Add files via upload
    queries with all 5 questions added

--- emp_manager.sql
+++ emp_manager.sql
@@ -18,3 +18,41 @@
  ('Joe',104),
  ('Akshiv',105);
  select * from manager;
+
+SELECT manager.manager_name, employee.empid, employee.fname, employee.lname
+FROM manager
+JOIN employee ON manager.empid = employee.empid
+ORDER BY manager.manager_name;
+
+SELECT manager_name, COUNT(empid) AS employee_count
+FROM manager
+WHERE manager_name = 'Akshay'
+GROUP BY manager_name;
+
+SELECT manager.manager_name, employee.empid, employee.fname, employee.lname
+FROM manager
+JOIN employee ON manager.empid = employee.empid
+ORDER BY manager.manager_name;
+
+SELECT employee.empid, employee.fname, employee.lname
+FROM employee
+LEFT JOIN manager ON employee.empid = manager.empid
+WHERE manager.empid IS NULL;
+
+DELIMITER $$
+
+CREATE FUNCTION get_full_name(emp_id INT)
+RETURNS VARCHAR(50)
+DETERMINISTIC
+BEGIN
+    DECLARE full_name VARCHAR(50);
+    
+    SELECT CONCAT(fname, ' ', lname) INTO full_name
+    FROM employee
+    WHERE empid = emp_id;
+    
+    RETURN full_name;
+END$$
+
+DELIMITER ;
+SELECT get_full_name(100);
